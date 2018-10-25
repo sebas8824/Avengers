d6d09c30098e0757ced9fbab7557003a26402bf9
Sebastián B <sebastian.benitez@outlook.com>
2018-10-25 16:45:05 -0500

    Update MarvelApi.swift

--- Avengers/Utils/Constants/MarvelApi.swift
+++ Avengers/Utils/Constants/MarvelApi.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 struct MarvelApi {
-    let publicKey = "0b1a701419d5b180e71a6f2f1c314e3a"
-    let privateKey = "5f13a8d0e66e8644cb19d2cbb4a0a5c24af53e66"
+    let publicKey = "your_publicKey"
+    let privateKey = "your_privateKey"
     let refererUrl = "developer.marvel.com"
     let gatewayUrl = "https://gateway.marvel.com:443/"
     let limit = "20"
